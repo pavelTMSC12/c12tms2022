dbd198f7415f420794264cd42d84d54eb946b060
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2022-01-17 23:32:23 +0300

    Лекция 2: практика

---
+++ Lesson2/src/main/java/Practice1.java
@@ -0,0 +1,38 @@
+public class Practice1 {
+    public static final String TEMP = "c:\\temp";
+
+
+    public static void main(String[] args) {
+
+        int myCount = 5 + 0b1111;
+        char c = 'v';
+
+        //int c = 1;
+        final int k = 1;
+        test();
+        int b = 1;
+        c = '1';
+        System.out.println(c);
+
+        float f = 2.2f;
+        double d = 4.4;
+
+        long c1 = 1231L;
+
+//    Byte, Short, Integer, Long, Float, Double, Character, Boolean
+
+//      float
+//      double
+        boolean b2 = false;//0
+        boolean b1 = true;//1
+
+        String str = "";
+        System.out.println("Привет \"Минск\" \n как дела");
+    }
+
+    private static void test() {
+        boolean b = false;//0
+        boolean b1 = true;//1
+        System.out.println("sdf");
+    }
+}
