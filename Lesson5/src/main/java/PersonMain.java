f0372b1271cdf9184ed339658172678b04423b01
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2022-02-01 00:47:07 +0300

    Лекция6: практика и домашка

---
+++ Lesson5/src/main/java/PersonMain.java
@@ -0,0 +1,5 @@
+public class PersonMain {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
--- Lesson6/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        do {
-
-        } while (5 > 9);
-
-
-    }
-}
